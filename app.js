460887e9b31269c2c8f995edda21ad66f0920c81
Brayan Castro P <brayancastrop@gmail.com>
2015-02-25 10:12:45 -0500

    fixing target name through deploys

--- app.js
+++ app.js
@@ -84,14 +84,12 @@ var setup = function () {
         repoUrl = data.repository.url;
         repoName = data.repository.name;
         organization = data.repository.organization;
-        name = projectRoot + '/tmp/' + repoName;
         targetUrl = 'git@github.com:' + organization + '/' + repoName;
         lastCommitInfo = data.commits[data.commits.length - 1];
         report = config.server.root + ':' + config.server.port + '/reports/' + data.repository.name + '/' + lastCommitInfo.id + '.html';
         target = {
             'branch': branch,
             'url': targetUrl,
-            'name': name,
             'organization': organization,
             'repoUrl': repoUrl,
             'repoName': repoName,
